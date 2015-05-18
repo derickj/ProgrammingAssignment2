5d8e7400adc9665c20db2fdfe82d35d82f4c04ae
Derick J <derick63@gmail.com>
2015-05-18 16:25:17 +0200

    For submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,27 +1,31 @@
-## R Programming Course rprog-14
+## R Programming Course: rprog-14
+##
 ## Programming Assignment 2
 ## The file contains two functions, the first creates a "matrix" object from
 ## an invertible matrix (input paramater), the second calculates the inverse
-## of the matrix (or returns the cache value if already calculated)
+## of the matrix (or returns the cached inverse if already calculated)
 ##
 ## Example usage: 
-##    mymatrx <- rbind(c(1,-1/4), c(-1/4,1))
-##    myspecialmatrix <- makeCacheMatrix(mymatrx)
-##    cacheSolve(myspecialmatrx)
-## 
+##    m <- rbind(c(1,-1/4), c(-1/4,1))
+##    mv <- makeCacheMatrix(m)
+##    cacheSolve(mv)   
+##    cacheSolve(mv)   
+##    mv$set(rbind(c(1,-1/4), c(-1/4,1)))
+##    cacheSolve(mv)   
 
 ## 
 ## makeCacheMatrix(matr)
 ## Arguments
-##    matr - an invertible matrix
-## Returns
+##    x - an invertible matrix
+## Return
 ##    The function returns a new object, which is actually a vector
 ##    containing 4 functions:
-##      set is used to set() (initialize) the object
+##      set is used to set() the object (only needed when it changes)
 ##      get is used to get() the original matrix (the original input)
 ##      setinv is used to set the matrix inverse (but in the closure of the
-##         object created by the function
-##      getinv simply return the matrx inverse stored in the object
+##         makeCacheMatrix function,i.e. it would change the inv value
+##      getinv simply return the matrix inverse stored in the object
+##         (NULL if this is the first call for the object in question)
 ##
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -36,30 +40,24 @@ makeCacheMatrix <- function(x = matrix()) {
     list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
 ## 
-## cacheSolve(specialmatr)
+## cacheSolve(vect)
 ## Arguments
-##    specialmatr - an special "matrix" or vector object created by makeCacheMatrix
-## Returns
-##    The function returns the inverse of the matrix. 
-##    If this is the first time cacheSolve is called for the specific object
-##    the inverse is calculated using the solve() function and then setinv
-##    is used on the object to cache the inverse value
-##    On subsequent calls, the inverse is merely returned as it has already 
-##    been calculated on the first call for a particular object.
+##    x - a special vector created by makeCacheMatrix()
+## Return
+##    Returns a matrix that is the inverse of the matrix
+##    from which x was constructed using makeCacheMatix
 ##
-
-
 cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
     inv <- x$getinv()
     if (!is.null(inv)) {
-        message("cached")
+        ## The inverse was already calculated and is simply returned
+        message("getting cached inverse")
         return(inv)
     }
-    data <- x$get()
-    inv <- solve(data)
-    x$setinv(inv)
-    inv
+    ## The inverse has not yet been calculated
+    data <- x$get()          ## Retrieve the original (input) matrix
+    inv <- solve(data)       ## Calculate the inverse
+    x$setinv(inv)            ## Update the inverse for the vector
+    inv                      ## Return the inverse
 }
